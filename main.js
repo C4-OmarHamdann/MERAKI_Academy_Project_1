0eda35f3afe7d89cb3fc9cebd5cdbc30e6f4c050
OmarHamdann <omar.haamdan@gmail.com>
2021-12-04 18:56:40 +0200

    test random image

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ let count = 0;
 console.log(move);
 //Add Click Listener to all card\\
 for (i = 0; i < cards.length; i++) {
+  console.log(cards[i].children[0].src);
+  console.log(
+    (cards[i].children[0].src =
+      "./images/" + Math.floor(Math.random() * 5) + ".jpg")
+  );
   cards[i].addEventListener("click", function () {
     move.innerText = ++count;
     this.children[0].classList.toggle("show");
