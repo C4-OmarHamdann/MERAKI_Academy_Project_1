2af62767196239677d19712aa44345f81a33439c
OmarHamdann <omar.haamdan@gmail.com>
2021-12-04 20:28:53 +0200

    Add timer

--- main.js
+++ main.js
@@ -2,12 +2,26 @@ let cards = document.querySelectorAll(".card");
 let move = document.querySelector(".count");
 let image = document.querySelectorAll("img");
 let count = 0;
-console.log(move);
+
 //Add Click Listener to all card\\
 for (i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", function () {
     move.innerText = ++count;
     this.children[0].classList.toggle("show");
+
+    //Timer\\
+
+    const timer = document.querySelector(".timer");
+    let numTimer = 10;
+
+    const startTimer = setInterval(() => {
+      timer.innerText = numTimer;
+      --numTimer;
+
+      if (numTimer == -1) {
+        clearInterval(startTimer);
+      }
+    }, 1000);
   });
 }
 
@@ -32,5 +46,3 @@ imageArray.forEach((element, i) => {
   let randomNum = Math.floor(Math.random() * 12);
   image[i].src = element;
 });
-
-//Timer\\
