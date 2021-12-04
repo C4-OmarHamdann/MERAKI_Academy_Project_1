5f831b4d4527e4b93c2b6cb497541d00d3a46f22
OmarHamdann <omar.haamdan@gmail.com>
2021-12-04 19:51:26 +0200

    Array image

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 let cards = document.querySelectorAll(".card");
 let move = document.querySelector(".count");
+let image = document.querySelectorAll("img");
 let count = 0;
 console.log(move);
 //Add Click Listener to all card\\
 for (i = 0; i < cards.length; i++) {
-  console.log(cards[i].children[0].src);
-  console.log(
-    (cards[i].children[0].src =
-      "./images/" + Math.floor(Math.random() * 5) + ".jpg")
-  );
   cards[i].addEventListener("click", function () {
     move.innerText = ++count;
     this.children[0].classList.toggle("show");
   });
 }
+
+//Array images random\\
+
+const imageArray = [
+  "./images/4.jpg",
+  "./images/1.jpg",
+  "./images/5.jpg",
+  "./images/3.jpg",
+  "./images/2.jpg",
+  "./images/0.jpg",
+  "./images/3.jpg",
+  "./images/2.jpg",
+  "./images/1.jpg",
+  "./images/5.jpg",
+  "./images/0.jpg",
+  "./images/4.jpg",
+];
+
+imageArray.forEach((element, i) => {
+  let randomNum = Math.floor(Math.random() * 12);
+  image[i].src = element;
+});
+
+//Timer\\
